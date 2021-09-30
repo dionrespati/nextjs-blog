8366858347182cdc5508eea8a6fc49f1a354349f
Dion Respati <dionrespati@gmail.com>
2021-09-30 13:27:31 +0700

    new branch

---
+++ pages/posts.jsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react';
+
+const posts = () => {
+	const [isValid, setValid] = useState(0);
+
+	useEffect(() => {    
+		fetch('https://jsonplaceholder.typicode.com/posts', {
+			method: 'POST',
+			body: JSON.stringify({
+				title: 'foo',
+				body: 'bar',
+				userId: 1,
+			}), headers: {
+    		'Content-type': 'application/json; charset=UTF-8',
+	   	},
+	  }).then((response) => response.json());
+		setValid(1); 
+	},[]);
+
+	return (
+		<div>
+			This is post user, please stop
+			Status {isValid}
+		</div>
+	);
+};
+
+export default posts;
